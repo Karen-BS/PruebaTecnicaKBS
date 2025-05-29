ccc0c3ddcb20b0e6421db1b1cbfc43f2270f99bd
Karen Bozziere Solis <karenbozziere@gmail.com>
2025-05-29 09:41:47 -0600

    Creación del controlador de pokemon

---
+++ src/main/java/com/example/pruebatecnicakbs/controller/PokemonController.java
@@ -0,0 +1,26 @@
+package com.example.pruebatecnicakbs.controller;
+
+import com.example.pruebatecnicakbs.service.PokemonService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/pokemon")
+public class PokemonController {
+    private final PokemonService pokemonService;
+
+    public PokemonController(PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
+    }
+
+    @GetMapping
+    public String obtenerPokemones(@RequestParam(defaultValue = "10") int limit,
+                                   @RequestParam(defaultValue = "0") int offset) {
+        return pokemonService.obtenerListadoPokemon(limit, offset);
+    }
+
+    @GetMapping("/{nombre}")
+    public String obtenerDetalle(@PathVariable String nombre) {
+        return pokemonService.obtenerDetallePokemon(nombre);
+    }
+}
