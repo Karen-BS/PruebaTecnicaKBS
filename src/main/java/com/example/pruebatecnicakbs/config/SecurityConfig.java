092a891b39517e1f4a9886e1d3f3c738d9691948
Karen Bozziere Solis <karenbozziere@gmail.com>
2025-05-29 10:29:29 -0600

    Implementación de seguridad con autenticación basica

---
+++ src/main/java/com/example/pruebatecnicakbs/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.pruebatecnicakbs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password("{noop}1234")
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+        return http.build();
+    }
+}
